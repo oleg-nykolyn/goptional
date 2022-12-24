ffdb154ce1aa029a285c9ad46edb6a61e23d3662
Oleg Nykolyn <o.nykolyn@eight-twenty.com>
2022-12-24 21:42:06 +0100

    isolate

--- goptional_test.go
+++ goptional_test.go
@@ -10,6 +10,5 @@ func TestEmpty(t *testing.T) {
 	opt := Empty[interface{}]()
 	require.NotNil(t, opt)
 	require.Nil(t, opt.wrappedValue)
-	require.True(t, opt.IsEmpty())
 	require.EqualValues(t, opt, Empty[interface{}]())
 }
