482ab5f898ffc3287f9c3eefc74fcfc3589e6d80
Oleg Nykolyn <o.nykolyn@eight-twenty.com>
2022-12-24 14:22:21 +0100

    Begin core api impl

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ The definitive generic-based implementation of the `Optional` type for Go 🚀
 
 ## Installation
 
-`go get -u github.com/nykolynoleg/goptional`
+```bash
+go get -u github.com/nykolynoleg/goptional
+```
--- goptional.go
+++ goptional.go
@@ -1,4 +1,91 @@
 package goptional
 
-// DoNothing does nothing.
-func DoNothing() {}
+import "reflect"
+
+type Optional[T any] struct {
+	wrappedValue *valueWrapper[T]
+}
+
+type valueWrapper[T any] struct {
+	value T
+}
+
+const noSuchElementErrMsg = "no value present"
+
+func Empty[T any]() *Optional[T] {
+	return &Optional[T]{}
+}
+
+func Of[T any](value T) *Optional[T] {
+	if reflect.ValueOf(&value).Elem().IsZero() {
+		return Empty[T]()
+	}
+	return &Optional[T]{
+		wrappedValue: &valueWrapper[T]{value: value},
+	}
+}
+
+func (o *Optional[T]) Get() T {
+	if !o.IsPresent() {
+		panic(noSuchElementErrMsg)
+	}
+	return o.wrappedValue.value
+}
+
+func (o *Optional[T]) IsPresent() bool {
+	return o.wrappedValue != nil
+}
+
+func (o *Optional[T]) IsEmpty() bool {
+	return o.wrappedValue == nil
+}
+
+func (o *Optional[T]) IfPresent(action func(T)) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o *Optional[T]) IfPresentOrElse(action func(T), runnable func()) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o *Optional[T]) Filter(predicate func(T) bool) *Optional[T] {
+	//TODO implement me
+	panic("implement me")
+}
+
+func Map[X, Y any](input *Optional[X], mapper func(X) Y) Optional[Y] {
+	//TODO implement me
+	panic("implement me")
+}
+
+func FlatMap[X, Y any](input *Optional[X], mapper func(X) *Optional[Y]) *Optional[Y] {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o *Optional[T]) Or(supplier func() Optional[T]) *Optional[T] {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o *Optional[T]) OrElse(other T) T {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o *Optional[T]) OrElseGet(supplier func() T) T {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o *Optional[T]) OrElsePanic() T {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o *Optional[T]) OrElsePanicWithErr(f func() error) T {
+	//TODO implement me
+	panic("implement me")
+}
