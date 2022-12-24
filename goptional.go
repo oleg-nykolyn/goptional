4178a07cd0024c23f8b00c08119ef6b3650ef76b
Oleg Nykolyn <o.nykolyn@eight-twenty.com>
2022-12-24 14:40:00 +0100

    provide untested api impl

--- goptional.go
+++ goptional.go
@@ -25,13 +25,6 @@ func Of[T any](value T) *Optional[T] {
 	}
 }
 
-func (o *Optional[T]) Get() T {
-	if !o.IsPresent() {
-		panic(noSuchElementErrMsg)
-	}
-	return o.wrappedValue.value
-}
-
 func (o *Optional[T]) IsPresent() bool {
 	return o.wrappedValue != nil
 }
@@ -40,52 +33,82 @@ func (o *Optional[T]) IsEmpty() bool {
 	return o.wrappedValue == nil
 }
 
-func (o *Optional[T]) IfPresent(action func(T)) {
-	//TODO implement me
-	panic("implement me")
+func (o *Optional[T]) Get() T {
+	if o.IsEmpty() {
+		panic(noSuchElementErrMsg)
+	}
+	return o.wrappedValue.value
 }
 
-func (o *Optional[T]) IfPresentOrElse(action func(T), runnable func()) {
-	//TODO implement me
-	panic("implement me")
+func (o *Optional[T]) IfPresent(action func(T)) {
+	if o.IsPresent() {
+		action(o.Get())
+	}
+}
+
+func (o *Optional[T]) IfPresentOrElse(action func(T), emptyAction func()) {
+	if o.IsPresent() {
+		action(o.Get())
+	} else {
+		emptyAction()
+	}
 }
 
 func (o *Optional[T]) Filter(predicate func(T) bool) *Optional[T] {
-	//TODO implement me
-	panic("implement me")
+	if o.IsEmpty() {
+		return o
+	}
+	if predicate(o.Get()) {
+		return o
+	}
+	return Empty[T]()
 }
 
-func Map[X, Y any](input *Optional[X], mapper func(X) Y) Optional[Y] {
-	//TODO implement me
-	panic("implement me")
+func Map[X, Y any](input *Optional[X], mapper func(X) Y) *Optional[Y] {
+	if input.IsEmpty() {
+		return Empty[Y]()
+	}
+	return Of(mapper(input.Get()))
 }
 
 func FlatMap[X, Y any](input *Optional[X], mapper func(X) *Optional[Y]) *Optional[Y] {
-	//TODO implement me
-	panic("implement me")
+	if input.IsEmpty() {
+		return Empty[Y]()
+	}
+	return mapper(input.Get())
 }
 
-func (o *Optional[T]) Or(supplier func() Optional[T]) *Optional[T] {
-	//TODO implement me
-	panic("implement me")
+func (o *Optional[T]) Or(supplier func() *Optional[T]) *Optional[T] {
+	if o.IsPresent() {
+		return o
+	}
+	return supplier()
 }
 
 func (o *Optional[T]) OrElse(other T) T {
-	//TODO implement me
-	panic("implement me")
+	if o.IsPresent() {
+		return o.Get()
+	}
+	return other
 }
 
 func (o *Optional[T]) OrElseGet(supplier func() T) T {
-	//TODO implement me
-	panic("implement me")
+	if o.IsPresent() {
+		return o.Get()
+	}
+	return supplier()
 }
 
 func (o *Optional[T]) OrElsePanic() T {
-	//TODO implement me
-	panic("implement me")
+	if o.IsEmpty() {
+		panic(noSuchElementErrMsg)
+	}
+	return o.Get()
 }
 
 func (o *Optional[T]) OrElsePanicWithErr(f func() error) T {
-	//TODO implement me
-	panic("implement me")
+	if o.IsEmpty() {
+		panic(f().Error())
+	}
+	return o.Get()
 }
